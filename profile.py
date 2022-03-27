274875c791df18fa959d385a2dbb2ec7db8846b5
arkivm <vikram186@gmail.com>
2022-03-26 23:02:51 -0700

    ksplit dev

--- profile.py
+++ profile.py
@@ -1,4 +1,4 @@
-"""LVD linux development on bareflank hypervisor"""
+"""kernel static analysis"""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -10,18 +10,43 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
+
+pc.defineParameter("DATASET", "URN of your image-backed dataset", 
+                   portal.ParameterType.STRING,
+                   "urn:publicid:IDN+emulab.net:xcap+ltdataset+vi")
+pc.defineParameter("MOUNTPOINT", "Mountpoint for file system",
+                   portal.ParameterType.STRING, "/local/device")
+
+pc.defineParameter("DISKIMAGE", "Disk image",
+                   portal.ParameterType.STRING,
+                   "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD")
+pc.defineParameter("HWTYPE", "Disk image",
+                   portal.ParameterType.STRING,
+                   "d820")
+
+
+params = pc.bindParameters()
+
 # Node node-0
 node_0 = request.RawPC('node-0')
-node_0.hardware_type = 'c220g2'
-node_0.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+lvds-PG0:lvd-linux-4.8.4-ubuntu18-04'
+node_0.hardware_type = params.HWTYPE
+node_0.disk_image = params.DISKIMAGE
 
-# Node node-1
-node_1 = request.RawPC('node-1')
-node_1.hardware_type = 'c220g2'
-node_1.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+lvds-PG0:lvd-linux-4.8.4-ubuntu18-04'
+iface = node_0.addInterface()
 
-link1 = request.Link(members = [node_0, node_1])
+# Node blockhost-idisk
+fsnode = request.RemoteBlockstore('fsnode', params.MOUNTPOINT)
+fsnode.dataset = params.DATASET
+
+
+# Now we add the link between the node and the special node
+fslink = request.Link("fslink")
+fslink.addInterface(iface)
+fslink.addInterface(fsnode.interface)
+
+# Special attributes for this link that we must use.
+fslink.best_effort = True
+fslink.vlan_tagging = True
 
 # Install and execute a script that is contained in the repository.
 #node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
